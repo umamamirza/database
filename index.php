5e39cae2475d376c216481e6949c576685de3665
unknown <umaimamirza369@gmail.com>
2025-01-16 15:19:51 +0500

    add file

--- index.php
+++ index.php
@@ -1,25 +1,18 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $target = "uploads/";
-    $destination = "archive/"; // Destination folder for moving files
-
-   
-    if (!is_dir($destination)) {
-        mkdir($destination, 0777, true);
-    }
-
+ 
     $file = $_FILES['image'];
     $fileName = basename($file['name']);
     $targetFile = $target . $fileName;
-    $destinationFile = $destination . $fileName;
     $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
-    
+   
     if (move_uploaded_file($file['tmp_name'], $targetFile)) {
-        echo "The file " . htmlspecialchars($fileName) . " has been uploaded successfully! .<br>";
+        echo "The file " . htmlspecialchars($fileName) . " has been uploaded successfully!";
     } else {
         echo "Error: There was a problem uploading your file.";
     }
-        // Move the file to another folder
+  // Move the file to another folder
         if (rename($targetFile, $destinationFile)) {
             echo "The file has been moved to the archive folder.<br>";
         } else {
